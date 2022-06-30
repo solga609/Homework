84725eaf88b4c7db25ca1183c3f342d52db2e924
Olga <olga609@mail.ru>
2022-06-30 14:16:50 +0300

    Задача номер 10

---
+++ Task_001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int rndNum = new Random(). Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {rndNum} ");
+int firstDigit = rndNum / 100;
+int secondDigit = rndNum % 100;
+int thirdDigit = secondDigit / 10;
+
+ Console.WriteLine(thirdDigit);
+
